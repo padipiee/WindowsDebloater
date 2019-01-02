56fdfaeb07c0ba7622cd7c5b876d1356f21f8ac8
blue101010 <blue101010>
2019-01-01 20:42:00 -0500

    Update comments for 0027

--- 0027_Delete_recusively_files_with_powershell.ps1
+++ 0027_Delete_recusively_files_with_powershell.ps1
@@ -1,3 +1,5 @@
-# Delete recusively files with powershell
+# Useful command to Delete  recusively files with powershell. 
+# Adapt path and filter accordingly
+
 
 Get-ChildItem -path E:\SCANS -Include *.part -Recurse | Remove-Item -Verbose
