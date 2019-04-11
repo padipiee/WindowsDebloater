3aad9ea5e1051f83cf170a53c9621aa09a6af11b
blue101010 <blue101010>
2019-04-10 23:09:22 -0400

    Add control 0038 for deactivating Bluetooth

---
+++ controls/batch/0038_Disable_Blutooth.bat
@@ -0,0 +1,4 @@
+echo | set /p=Disable Bluetooth 
+
+REM change the value of Type DWORD from 0 to 1 to deactivate Bluetooth
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\ActionCenter\Quick Actions\All\SystemSettings_Device_BluetoothQuickAction" /v "Type" /t REG_DWORD /d "1" /f
