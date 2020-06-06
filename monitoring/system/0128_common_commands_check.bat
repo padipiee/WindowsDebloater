0fadd618ce99c7302d46148bd831d5c51174ce97
blue101010 <blue101010>
2020-06-06 14:14:47 -0400

    2 controls added

---
+++ monitoring/system/0128_common_commands_check.bat
@@ -0,0 +1,31 @@
+gpresult /z
+grpresult /v
+net view
+net view /domain
+netstat 
+netstat -nab
+netstat -nao
+nslookup 127.0.0.1
+ipconfig /all
+arp -a
+net share
+net use
+systeminfo
+net user
+net user administrator
+net user /domain
+net group
+net group /domain
+net localgroup
+net localgroup Administrators
+dir %programfiles%
+met accounts
+net accounts /domain
+net view 127.0.0.1 /all
+net session
+route print
+ipconfig /displaydns
+dir /og-d "%userprofile%\AppData\Roaming\Microsoft\Windows\Recent\*.*" 
+
+
+
---
+++ security/default/batch/0127_Disable_Wdigest_UseLogonCredential.bat
@@ -0,0 +1,16 @@
+
+
+::After you install this security update, you can control how installed WDigest credentials can be saved by using a registry setting. To prevent WDigest credentials from being stored in memory, a Group Policy setting can be applied to the UseLogonCredential registry entry under the following subkey:
+:: HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\WDigest
+:: If the UseLogonCredential value is set to 0, WDigest will not store credentials in memory.
+:: https://support.microsoft.com/en-us/help/2871997/microsoft-security-advisory-update-to-improve-credentials-protection-a
+
+::Digest Authentication is a challenge/response protocol that was primarily used in Windows Server 2003 for LDAP and web-based authentication. 
+::It utilizes Hypertext Transfer Protocol (HTTP) and Simple Authentication Security Layer (SASL) exchanges to authenticate.
+:: https://blog.stealthbits.com/wdigest-clear-text-passwords-stealing-more-than-a-hash/
+
+::https://www.stigviewer.com/stig/windows_7/2016-12-19/finding/V-72753
+
+reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 0 /f
+
+reg query HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential
