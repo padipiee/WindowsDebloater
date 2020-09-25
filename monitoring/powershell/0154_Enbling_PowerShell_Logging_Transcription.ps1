e6486239ace9e1d876c7dd58b448676ddb6d128c
blue101010 <blue101010>
2020-09-24 22:39:52 -0400

    Add 0153/0154 : Powershell logging

---
+++ monitoring/powershell/0154_Enbling_PowerShell_Logging_Transcription.ps1
@@ -0,0 +1,30 @@
+#https://adamtheautomator.com/powershell-logging-recording-and-auditing-all-the-things/
+
+function Enable-PSTranscriptionLogging {
+	param(
+		[Parameter(Mandatory)]
+		[string]$OutputDirectory
+	)
+
+     # Registry path
+     $basePath = 'HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\Transcription'
+
+     # Create the key if it does not exist
+     if(-not (Test-Path $basePath))
+     {
+         $null = New-Item $basePath -Force
+
+         # Create the correct properties
+         New-ItemProperty $basePath -Name "EnableInvocationHeader" -PropertyType Dword
+         New-ItemProperty $basePath -Name "EnableTranscripting" -PropertyType Dword
+         New-ItemProperty $basePath -Name "OutputDirectory" -PropertyType String
+     }
+
+     # These can be enabled (1) or disabled (0) by changing the value
+     Set-ItemProperty $basePath -Name "EnableInvocationHeader" -Value "1"
+     Set-ItemProperty $basePath -Name "EnableTranscripting" -Value "1"
+     Set-ItemProperty $basePath -Name "OutputDirectory" -Value $OutputDirectory
+
+}
+
+Enable-PSTranscriptionLogging
