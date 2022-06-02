87d489cfe2b3f98ae44476de1dc55ee0d34290a4
blue101010 <blue101010>
2022-06-01 22:05:48 -0400

    renaming standardization and add CVE-2022-30190

---
+++ security/default/0241_Disable_ms-msdt.bat
@@ -0,0 +1,7 @@
+::Disable UAC remote elevation capability
+
+::CVE-2022-30190
+::https://msrc-blog.microsoft.com/2022/05/30/guidance-for-cve-2022-30190-microsoft-support-diagnostic-tool-vulnerability/
+reg export HKEY_CLASSES_ROOT\ms-msdt CVE-2022-30190
+
+reg delete HKEY_CLASSES_ROOT\ms-msdt /f
