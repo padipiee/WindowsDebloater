6f3dbaabbfbcedc0cf09277379534282b9519645
blue101010 <blue101010>
2022-06-07 22:39:08 -0400

    0248_Get-EventLog-security

--- security/default/0232_get-evenlog_application.ps1
+++ security/default/0232_get-evenlog_application.ps1
@@ -2,7 +2,3 @@
 mkdir c:\diag
 get-eventlog application | sort-object timegenerated | select-object      timegenerated,entrytype,machinename,eventid,source,username,message
 #get-eventlog application |      sort-object timegenerated | select-object      timegenerated,entrytype,machinename,eventid,source,username,message |      export-csv c:\diag\application.csv -notype
-
-#Latest detailed 50 security events
-Get-EventLog -LogName 'Security'  -Newest 50 |Format-List
-#https://evotec.xyz/powershell-everything-you-wanted-to-know-about-event-logs/
