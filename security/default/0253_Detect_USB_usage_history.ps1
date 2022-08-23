bcf037a64867b39ef915e7e3ef9573e49f7012f0
blue101010 <blue101010>
2022-08-22 21:30:29 -0400

    usb event trace updates

---
+++ security/default/0253_Detect_USB_usage_history.ps1
@@ -0,0 +1,12 @@
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR\*\*' | Select FriendlyName
+
+###To capture a USB event trace
+##Check https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/how-to-capture-a-usb-event-trace an usage of logman an perfview
+
+###Exemple
+# logman create trace -n usbtrace -o %SystemRoot%\Tracing\usbtrace.etl -nb 128 640 -bs 128
+# logman update trace -n usbtrace -p Microsoft-Windows-USB-USBXHCI (Rundown,Power)
+# logman update trace -n usbtrace -p Microsoft-Windows-USB-UCX (Rundown,Power)
+# logman update trace -n usbtrace -p Microsoft-Windows-USB-USBHUB3 (Rundown,Power)
+# logman update trace -n usbtrace -p Microsoft-Windows-Kernel-IoTrace 0 2
+# logman start -n usbtrace
