532626bb67de049ad3fb372fedface91c8a4177a
blue101010 <none@hello.com>
2024-10-05 15:27:44 -0400

    fix quotes

--- security/default/0299_Enforce_screen_locking.bat
+++ security/default/0299_Enforce_screen_locking.bat
@@ -5,5 +5,4 @@
 @REM powercfg.exe /setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK 300
 @REM powercfg.exe /setactive SCHEME_CURRENT
 
-@REM #cmd 
-@REM REG ADD 'HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7' /v Attributes /t REG_DWORD /d 00000002 /
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 00000002
