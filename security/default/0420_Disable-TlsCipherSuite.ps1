a8d1570d8e6807fbf6b51a70b838db917c3a57cd
blue101010 <none@hello.com>
2025-01-27 22:56:50 -0500

    Enhance security by disabling additional TLS cipher suites, including 3DES, RC4, anonymous DH, and MD5-based suites.

--- security/default/0420_Disable-TlsCipherSuite.ps1
+++ security/default/0420_Disable-TlsCipherSuite.ps1
@@ -18,3 +18,23 @@ Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_256_GCM_SHA384"
 Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_128_GCM_SHA256"
 Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_256_CBC_SHA384"
 Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_128_CBC_SHA256" 
+
+# Disable 3DES (Triple DES) cipher suites
+Disable-TlsCipherSuite -Name "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+Disable-TlsCipherSuite -Name "TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA"
+Disable-TlsCipherSuite -Name "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
+
+# Disable RC4-based cipher suites
+Disable-TlsCipherSuite -Name "TLS_RSA_WITH_RC4_128_SHA"
+Disable-TlsCipherSuite -Name "TLS_RSA_WITH_RC4_128_MD5"
+Disable-TlsCipherSuite -Name "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
+Disable-TlsCipherSuite -Name "TLS_ECDHE_RSA_WITH_RC4_128_SHA"
+
+# Disable anonymous DH suites (no authentication)
+Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_RC4_128_MD5"
+Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_AES_256_CBC_SHA256"
+Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_AES_128_CBC_SHA256"
+
+# Disable MD5-based suites
+Disable-TlsCipherSuite -Name "TLS_RSA_WITH_NULL_MD5"
+Disable-TlsCipherSuite -Name "TLS_RSA_EXPORT_WITH_RC4_40_MD5"
