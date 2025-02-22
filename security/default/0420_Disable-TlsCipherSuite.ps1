d3454708487f5e0bcf40288bdbb88caed188965c
blue101010 <no@email.com>
2025-02-22 09:42:33 -0500

    0420_Disable-TlsCipherSuite

--- security/default/0420_Disable-TlsCipherSuite.ps1
+++ security/default/0420_Disable-TlsCipherSuite.ps1
@@ -1,40 +1,58 @@
 # Disable NULL Cipher Suites - 1
-Disable-TlsCipherSuite TLS_RSA_WITH_NULL_SHA256
+function DisableAndReport {
+    param($CipherSuiteName)
+    try {
+        Disable-TlsCipherSuite -Name $CipherSuiteName
+        Write-Host "Disabled $CipherSuiteName"
+    } catch {
+        if ($_.Exception.Message -like "*0xD0000225*") {
+            Write-Host "Cipher suite $CipherSuiteName is already disabled. See https://stackoverflow.com/questions/65213393/why-do-i-get-exception-from-hresult-0xd0000225-when-running-disable-tlsciphersu"
+        } else {
+            Write-Host "Failed to disable $CipherSuiteName. $_"
+        }
+    }
+}
+
+DisableAndReport "TLS_RSA_WITH_NULL_SHA256"
 # Disable NULL Cipher Suites - 2
-Disable-TlsCipherSuite TLS_RSA_WITH_NULL_SHA
+DisableAndReport "TLS_RSA_WITH_NULL_SHA"
 # Disable NULL Cipher Suites - 3
-Disable-TlsCipherSuite TLS_PSK_WITH_NULL_SHA384
+DisableAndReport "TLS_PSK_WITH_NULL_SHA384"
 # Disable NULL Cipher Suites - 4
-Disable-TlsCipherSuite TLS_PSK_WITH_NULL_SHA256
+DisableAndReport "TLS_PSK_WITH_NULL_SHA256"
 
 # disabling weak cipher suites
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_256_GCM_SHA384"
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_128_GCM_SHA256"
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_256_CBC_SHA256" 
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_128_CBC_SHA256"
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_256_CBC_SHA"
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_AES_128_CBC_SHA"
-Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_256_GCM_SHA384" 
-Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_128_GCM_SHA256"
-Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_256_CBC_SHA384"
-Disable-TlsCipherSuite -Name "TLS_PSK_WITH_AES_128_CBC_SHA256" 
+DisableAndReport "TLS_RSA_WITH_AES_256_GCM_SHA384"
+DisableAndReport "TLS_RSA_WITH_AES_128_GCM_SHA256"
+DisableAndReport "TLS_RSA_WITH_AES_256_CBC_SHA256"
+DisableAndReport "TLS_RSA_WITH_AES_128_CBC_SHA256"
+DisableAndReport "TLS_RSA_WITH_AES_256_CBC_SHA"
+DisableAndReport "TLS_RSA_WITH_AES_128_CBC_SHA"
+DisableAndReport "TLS_PSK_WITH_AES_256_GCM_SHA384"
+DisableAndReport "TLS_PSK_WITH_AES_128_GCM_SHA256"
+DisableAndReport "TLS_PSK_WITH_AES_256_CBC_SHA384"
+DisableAndReport "TLS_PSK_WITH_AES_128_CBC_SHA256"
 
 # Disable 3DES (Triple DES) cipher suites
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
-Disable-TlsCipherSuite -Name "TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA"
-Disable-TlsCipherSuite -Name "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
+DisableAndReport "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+DisableAndReport "TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA"
+DisableAndReport "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
 
 # Disable RC4-based cipher suites
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_RC4_128_SHA"
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_RC4_128_MD5"
-Disable-TlsCipherSuite -Name "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
-Disable-TlsCipherSuite -Name "TLS_ECDHE_RSA_WITH_RC4_128_SHA"
+DisableAndReport "TLS_RSA_WITH_RC4_128_SHA"
+DisableAndReport "TLS_RSA_WITH_RC4_128_MD5"
+DisableAndReport "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
+DisableAndReport "TLS_ECDHE_RSA_WITH_RC4_128_SHA"
 
 # Disable anonymous DH suites (no authentication)
-Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_RC4_128_MD5"
-Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_AES_256_CBC_SHA256"
-Disable-TlsCipherSuite -Name "TLS_DH_anon_WITH_AES_128_CBC_SHA256"
+DisableAndReport "TLS_DH_anon_WITH_RC4_128_MD5"
+DisableAndReport "TLS_DH_anon_WITH_AES_256_CBC_SHA256"
+DisableAndReport "TLS_DH_anon_WITH_AES_128_CBC_SHA256"
 
 # Disable MD5-based suites
-Disable-TlsCipherSuite -Name "TLS_RSA_WITH_NULL_MD5"
-Disable-TlsCipherSuite -Name "TLS_RSA_EXPORT_WITH_RC4_40_MD5"
+DisableAndReport "TLS_RSA_WITH_NULL_MD5"
+DisableAndReport "TLS_RSA_EXPORT_WITH_RC4_40_MD5"
+
+DisableAndReport "TLS_DHE_RSA_WITH_CAMELLIA_256_CBC_SHA"
+DisableAndReport "TLS_DHE_RSA_WITH_CAMELLIA_128_CBC_SHA"
+DisableAndReport "TLS_DHE_RSA_WITH_ARIA_256_GCM_SHA384"
