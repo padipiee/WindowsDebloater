6d0d69347ed022ae5e31b441da386854ee5e1151
blue101010 <no@email.com>
2025-03-02 18:46:46 -0500

    EnableKernelModeStackProtection improvement

--- security/default/0418_EnableKernelModeStackProtection.ps1
+++ security/default/0418_EnableKernelModeStackProtection.ps1
@@ -1,6 +1,12 @@
 # https://www.bleepingcomputer.com/tutorials/how-to-enable-kernel-mode-hardware-enforced-stack-protection-in-windows-11/
 # Enable Kernel-mode Hardware-enforced Stack Protection
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "EnableKernelModeStackProtection" -Value 1
+$currentValue = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "EnableKernelModeStackProtection" -ErrorAction SilentlyContinue)."EnableKernelModeStackProtection"
+if ($currentValue -ne 1) {
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "EnableKernelModeStackProtection" -Value 1
+    Write-Host "Kernel-mode hardware-enforced stack protection enabled."
+} else {
+    Write-Host "Kernel-mode hardware-enforced stack protection is already enabled."
+}
 
 # Restart the computer to apply the changes
 #Restart-Computer
